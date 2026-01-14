8aa3a1dce5a5de8fc981b85c3eea2e39203a9565
ManishSingh1810 <manishsinghh1810@gmail.com>
2026-01-14 14:11:13 +1000

    Update userContext.jsx

--- src/Context/userContext.jsx
+++ src/Context/userContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext, useEffect } from "react";
-// import api from "../../api/axios";
+import api from "../api/axios";
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
@@ -85,6 +85,7 @@ export const UserProvider = ({ children }) => {
 };
 
 export const useUser = () => useContext(UserContext);
+
 
 
 
